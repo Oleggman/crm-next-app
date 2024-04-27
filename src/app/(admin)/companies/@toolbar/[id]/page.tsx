9e45b1d56ab456ced79e47c7fbb85d1dd1785680
Олег <olegkovalets1112003@knu.ua>
2024-04-27 19:21:05 +0300

    Fix trial 3

--- src/app/(admin)/companies/@toolbar/[id]/page.tsx
+++ src/app/(admin)/companies/@toolbar/[id]/page.tsx
@@ -3,6 +3,13 @@ import Header from '@/app/components/header';
 
 export interface PageProps {}
 
+export async function generateStaticParams() {
+  const ids = ['1', '2', '3'];
+  return ids.map((id) => ({
+    params: { id },
+  }));
+}
+
 export default function Page({}: PageProps) {
   return null;
 }
